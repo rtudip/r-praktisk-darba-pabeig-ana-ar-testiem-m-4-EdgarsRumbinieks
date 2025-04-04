9e8b39652d159d15d5153bfa08b2fef1becbaf3d
EdgarsRumbinieks <144246668+EdgarsRumbinieks@users.noreply.github.com>
2025-04-04 11:44:45 +0000

    2nd try

--- programma.R
+++ programma.R
@@ -1,46 +1,34 @@
-setwd("C:/Users/37126/Desktop/Pr_darbs")
+#setwd("C:/Users/37126/Desktop/Pr_darbs")
 
-#1. punkts
-kordat <- read.csv("variants9.txt", header = T, dec = ",", sep = "")
+kordat <- read.csv("variants9.txt", header = T, dec = ",", sep = "", strip.white = T)
 
-#2. punkts
-kordat[,9:ncol(kordat)] <- lapply(kordat[,9:ncol(kordat)], as.factor)
+kordat[,9:ncol(kordat)] <- lapply(kordat[,9:ncol(kordat)],as.factor)
 
-#3. punkts
 sink("results.txt")
 
-#4. punkts
 summary(kordat[,9:ncol(kordat)])
 
-#5. punkts
-sl.by.b <- tapply(kordat$Slope, kordat$b,mean)
+sl.by.b <- split(kordat, kordat$b)
 print(sl.by.b)
 
-#6. punkts
 kordat$Average <- apply(kordat[,c(1,2,6)],1,mean)
 
-#7. punkts
-apply(kordat[,c(1:8,12)],2, function(x)(tapply(x,kordat$f,sd)))
+sapply(kordat[,c(1:8,12)], function(x)(tapply(x,kordat$f,sd)))
 
-#8. punkts
-#adj.r.squared nav nevienas vērtības >0.7, tāpēc nomainīju to uz >-0.4
-prockordat <- kordat[kordat$adj.r.squared>-0.4,]
+prockordat <- kordat[kordat$adj.r.squared>-0.7,]
 
-#9. punkts
-prockordat$Slope <- 1-1/prockordat$Slope
+funkc <- function(x) {1 - 1 / x}
+prockordat$Slope <- funkc(prockordat$Slope)
 
-#10. punkts
 print(prockordat)
 
 sink()
 
-#11. punkts
 svg("scatter.svg")
 plot(kordat$MAD,kordat$Average, main = "Grafiks", xlab = "MAD", ylab = "Average")
 dev.off()
 
-#12.punkts
 svg("boxplot.svg")
-boxplot(kordat$Intercept ~ kordat$f, main = "Kastes", xlab = "f faktors", ylab = "Intercept",
+boxplot(split(kordat$Intercept, kordat$f), main = "Kastes", xlab = "f faktors", ylab = "Intercept",
         col = c("blue","red",rep("blue",4)))
 dev.off()
